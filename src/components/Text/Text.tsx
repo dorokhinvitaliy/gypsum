4875dc33f516eeb7f537be04eef674842385ecc5
dorokhinvitaliy <dorokhin2006@bk.ru>
2025-09-01 11:19:47 +0300

    feat new variants added to text component

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,13 +1,37 @@
-export type variant = 'heading' | 'subheading' | 'body' | 'hint';
+export type TextVariant =
+  | 'display-1'
+  | 'display-2'
+  | 'display-3'
+  | 'display-4'
+  | 'heading-1'
+  | 'heading-2'
+  | 'heading-3'
+  | 'subheading-1'
+  | 'subheading-2'
+  | 'body-1'
+  | 'body-2'
+  | 'caption'
+  | 'overline'
+  | 'hint-1'
+  | 'hint-2';
 
+import type { HTMLAttributes } from 'react';
 import styles from './Text.module.css';
+import classNames from 'classnames';
 
 export default function Text({
   children,
-  variant = 'body',
+  variant = 'body-1',
+  className,
+  ...rest
 }: {
   children: React.ReactNode;
-  variant?: variant;
-}) {
-  return <span className={styles['text-variant-' + variant]}>{children}</span>;
+  variant?: TextVariant;
+  className?: string;
+} & HTMLAttributes<HTMLSpanElement>) {
+  return (
+    <span className={classNames(styles['text-variant-' + variant], className)} {...rest}>
+      {children}
+    </span>
+  );
 }
