b2bde893fa8003bf3e52e8b27604889aebfbf6b1
dorokhinvitaliy <dorokhin2006@bk.ru>
2025-09-07 01:36:27 +0300

    feat button themes added

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,14 @@
 import classNames from 'classnames';
 import React from 'react';
-import styles from './Button.module.css';
+import styles from './Button.module.scss';
+
+type ButtonTheme = 'primary' | 'secondary' | 'normal' | 'normal-flat' | 'flat' | 'outlined';
 
 export type ButtonProps = {
   children: React.ReactNode;
-  secondary?: boolean;
-  flat?: boolean;
-  long?: boolean;
+  theme?: ButtonTheme;
+  size?: 'xl' | 'l' | 'm' | 's' | 'xs';
+  width?: 'auto' | 'max';
   disabled?: boolean;
   loading?: boolean;
   className?: string;
@@ -14,9 +16,9 @@ export type ButtonProps = {
 
 export default function Button({
   children,
-  secondary = false,
-  flat = false,
-  long = false,
+  theme = 'primary',
+  size = 'l',
+  width,
   disabled = false,
   className = '',
   loading = false,
@@ -26,11 +28,15 @@ export default function Button({
     <button
       className={classNames(
         styles.button,
+        styles['theme-' + theme],
+        styles['size-' + size],
         { [styles.disabled]: disabled },
         { [styles.loading]: loading },
-        { [styles.secondary]: secondary },
-        { [styles.flat]: flat },
-        { [styles.long]: long },
+        {
+          [styles.iconOnly]:
+            React.isValidElement(children) && (children.type as any).displayName === 'Icon',
+        },
+        { [styles.long]: width === 'max' },
         className,
       )}
       {...props}
