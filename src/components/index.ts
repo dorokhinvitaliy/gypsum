a6368255925c9dc0adbd872c6ff5dd95f5203c05
dorokhinvitaliy <dorokhin2006@bk.ru>
2025-09-07 01:42:25 +0300

    feat structure of components

---
+++ src/components/index.ts
@@ -0,0 +1,21 @@
+// UI basics
+export { default as Button } from './Button/Button';
+export { default as Input } from './Input/Input';
+export { default as Select } from './Input/Select';
+export { default as Textarea } from './Input/Textarea';
+export { default as Text } from './Text/Text';
+export { default as Flex } from './Flex/Flex';
+export { default as Modal } from './Modal/Modal';
+export { default as Loader } from './Loader/Loader';
+export { default as Card } from './Card/Card';
+export { default as Aside } from './Aside/Aside';
+export { default as Layout } from './Layout/Layout';
+
+export { default as Tooltip } from './Tooltip/Tooltip';
+export { default as Checkbox } from './Checkbox/Checkbox';
+export { default as Disclosure } from './Disclosure/Disclosure';
+export { default as Icon } from './Icons/Icon/Icon';
+
+// Complex widgets
+export { default as Code } from './Code/Code';
+export { default as ColorPicker } from './ColorPicker/ColorPicker';
