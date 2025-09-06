c61ac0db56bc9c5f7946151360d696e7f56468c0
dorokhinvitaliy <dorokhin2006@bk.ru>
2025-09-07 01:37:19 +0300

    feat CodeBlock

---
+++ src/components/Code/Code.tsx
@@ -0,0 +1,35 @@
+import classNames from 'classnames';
+import { Highlight, themes } from 'prism-react-renderer';
+
+import styles from './Code.module.scss';
+import CopyOutline from '../Icons/Copy';
+import Icon from '../Icons/Icon/Icon';
+import Button from '../Button';
+
+const Code = ({ code }: { code: string }) => (
+  <div className={styles.codeBlock_wrapper}>
+    <Button size="s" theme="normal-flat" className={styles.copy}>
+      <Icon data={CopyOutline} />
+      Копировать
+    </Button>
+
+    <Highlight code={code.trim()} language="tsx" theme={themes.gruvboxMaterialLight}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre
+          className={classNames(className, styles.codeBlock)}
+          style={{ ...style, padding: '2rem', borderRadius: '1rem', margin: 0 }}
+        >
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line })}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  </div>
+);
+
+export default Code;
