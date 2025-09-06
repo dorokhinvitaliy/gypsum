7f4184c876918b11384940add9b1e92d34837bfa
dorokhinvitaliy <dorokhin2006@bk.ru>
2025-09-07 01:42:38 +0300

    fix many fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Animator from './pages/Animator/Animator';
+import Demo from './pages/Demo/Demo';
+import { Layout } from '@/components';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/animator" element={<Animator />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/animator" element={<Animator />} />
+        <Route path="/demo" element={<Demo />} />
+      </Routes>
+    </Layout>
   );
 }
 
