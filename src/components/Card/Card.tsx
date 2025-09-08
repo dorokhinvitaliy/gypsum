4523b764510086902f441a5d4b4a7e45cbaa998d
dorokhinvitaliy <dorokhin2006@bk.ru>
2025-09-08 11:52:48 +0300

    feat new card themes

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,19 +4,38 @@ import { Col } from '../Flex/Flex';
 import type { FlexProps } from '../Flex/Flex';
 import classNames from 'classnames';
 
+type CSSWithVars = React.CSSProperties & {
+  [key: `--${string}`]: string | number | undefined;
+};
+
 const Card = ({
   children,
   className,
+  theme = 'normal',
+  outlined,
   style,
+  brandColor,
   ...rest
 }: {
   children: React.ReactNode;
   className?: string;
+  theme?: 'normal' | 'normal-branded' | 'filled';
+  outlined?: boolean;
   style?: CSSProperties;
+  brandColor?: string;
 } & HTMLAttributes<HTMLElement> &
   FlexProps) => {
   return (
-    <Col className={classNames(styles.card, className)} style={{ ...style }} {...rest}>
+    <Col
+      className={classNames(
+        styles.card,
+        styles['card-theme-' + theme],
+        { [styles.outlined]: outlined },
+        className,
+      )}
+      style={{ '--gy-color-brand': brandColor, ...style } as CSSWithVars}
+      {...rest}
+    >
       {children}
     </Col>
   );
