16a505d686bd1ef85c783dda2600482ee798e343
dorokhinvitaliy <dorokhin2006@bk.ru>
2025-09-08 11:51:45 +0300

    fix components imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,11 +5,15 @@ export { default as Select } from './Input/Select';
 export { default as Textarea } from './Input/Textarea';
 export { default as Text } from './Text/Text';
 export { default as Flex } from './Flex/Flex';
+export { default as Col } from './Flex/Flex';
 export { default as Modal } from './Modal/Modal';
 export { default as Loader } from './Loader/Loader';
 export { default as Card } from './Card/Card';
-export { default as Aside } from './Aside/Aside';
+export { default as Aside } from './Navigation/Navigation';
+export { default as Anchor } from './Navigation/Anchor';
 export { default as Layout } from './Layout/Layout';
+export { default as Helper } from './Helper/Helper';
+export { default as Article } from './Article/Article';
 
 export { default as Tooltip } from './Tooltip/Tooltip';
 export { default as Checkbox } from './Checkbox/Checkbox';
